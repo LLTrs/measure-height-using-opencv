ac3232a658009dbf3674b7d327679d04592a02f0
LLTrs <110257555+LLTrs@users.noreply.github.com>
2021-04-16 09:24:05 +0000

    Update Documentation - What is new in Ring 1.14?

--- index.js
+++ index.js
@@ -25,4 +25,6 @@ export default function (kibana) {
     init(server, options) { // eslint-disable-line no-unused-vars
       // Add server routes and initialize the plugin here
       exampleRoute(server);
-    
+    }
+  });
+}
