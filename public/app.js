7928080222b3b0f704569654b2d414f3553cb1c6
LLTrs <110257555+LLTrs@users.noreply.github.com>
2021-08-08 11:51:45 +0000

    updated dev deps

--- public/app.js
+++ public/app.js
@@ -1,4 +1,4 @@
 import React from 'react';
 import { uiModules } from 'ui/modules';
 import chrome from 'ui/chrome';
-import { render, unmountComponentAt
+import { render, unmountComponentAtNode } from 'rea
