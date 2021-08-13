023d9a849ccdb7b43d8e5597e79ac93158220d82
LLTrs <110257555+LLTrs@users.noreply.github.com>
2021-08-13 19:12:11 +0000

    Roll back

--- public/app.js
+++ public/app.js
@@ -2,4 +2,6 @@ import React from 'react';
 import { uiModules } from 'ui/modules';
 import chrome from 'ui/chrome';
 import { render, unmountComponentAtNode } from 'react-dom';
-import { I18nProvider } from '@kbn/i18n/react'
+import { I18nProvider } from '@kbn/i18n/react';
+
+import 'ui/
