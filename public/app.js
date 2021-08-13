4400d7d00e44c3037cac2952b6df63538a4cc718
LLTrs <110257555+LLTrs@users.noreply.github.com>
2021-08-13 16:03:59 +0000

    Bump node-notifier from 8.0.0 to 8.0.1

--- public/app.js
+++ public/app.js
@@ -2,4 +2,4 @@ import React from 'react';
 import { uiModules } from 'ui/modules';
 import chrome from 'ui/chrome';
 import { render, unmountComponentAtNode } from 'react-dom';
-import { I18nProvider } from
+import { I18nProvider } from '@kbn/i18n/react'
