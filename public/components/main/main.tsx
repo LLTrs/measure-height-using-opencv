49d57e98f66cf1808c7e5343e8318c8498bb203b
LLTrs <110257555+LLTrs@users.noreply.github.com>
2021-11-12 08:07:20 +0000

    Heroku fix

--- public/components/main/main.tsx
+++ public/components/main/main.tsx
@@ -35,4 +35,4 @@ class ChartComponent extends React.Component {
 				{type => <Chart type={type} data={this.state.data} />}
       </TypeChooser>
       */
-     <Chart type="hybrid" data={this.
+     <Chart type="hybrid" data={this.state.data} /
