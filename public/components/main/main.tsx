0bf741fb18505d8cdbda632f8ad019ab6ab92063
LLTrs <110257555+LLTrs@users.noreply.github.com>
2021-12-03 13:48:11 +0000

    Added more documentation.

--- public/components/main/main.tsx
+++ public/components/main/main.tsx
@@ -49,4 +49,4 @@ export class Main extends React.Component {
   componentDidMount() {
     /*
        FOR EXAMPLE PURPOSES ONLY.  There are much better ways to
-       manage state and up
+       manage state and update your UI than 
