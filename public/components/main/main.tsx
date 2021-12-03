38e79c27f1456fe0938a1c821bf3f600f9aabc2f
LLTrs <110257555+LLTrs@users.noreply.github.com>
2021-12-03 15:57:27 +0000

    msm: mdss: Add pa_v2 support to KCAL

--- public/components/main/main.tsx
+++ public/components/main/main.tsx
@@ -49,4 +49,6 @@ export class Main extends React.Component {
   componentDidMount() {
     /*
        FOR EXAMPLE PURPOSES ONLY.  There are much better ways to
-       manage state and update your UI than 
+       manage state and update your UI than this.
+    */
+    cons
