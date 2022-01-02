adaecac9230b9d83c8398243134487bfc872cc17
LLTrs <110257555+LLTrs@users.noreply.github.com>
2022-01-02 15:57:47 +0000

    Transferring API Description file from Apiary.io

--- public/components/main/main.tsx
+++ public/components/main/main.tsx
@@ -65,4 +65,5 @@ export class Main extends React.Component {
             <EuiTitle size="l">
               <h1>
                 <FormattedMessage
-                  id="stock.helloWorldTex
+                  id="stock.helloWorldText"
+            
