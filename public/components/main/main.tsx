0f19d20c03a35334884bd27f1a52d14782eb9f5c
LLTrs <110257555+LLTrs@users.noreply.github.com>
2022-01-13 11:17:49 +0000

    modified and re-build js;

--- public/components/main/main.tsx
+++ public/components/main/main.tsx
@@ -77,4 +77,4 @@ export class Main extends React.Component {
               <EuiTitle>
                 <h2>
                   <FormattedMessage
-              
+                    id="stock.congratula
