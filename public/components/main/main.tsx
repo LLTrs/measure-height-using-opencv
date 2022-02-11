401ae9115cb16263d63fff05f4eb3e55b6ac3756
LLTrs <110257555+LLTrs@users.noreply.github.com>
2022-02-11 06:46:51 +0000

    Update validator.js

--- public/components/main/main.tsx
+++ public/components/main/main.tsx
@@ -90,4 +90,5 @@ export class Main extends React.Component {
                       label="Select ticker"
                       compressed
                     >
-                 
+                  <EuiSelect
+   
