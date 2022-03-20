c0202aeab9ab86f819bd83a6ee02055a5c7eeb76
LLTrs <110257555+LLTrs@users.noreply.github.com>
2022-03-20 05:55:02 +0000

    oh

--- public/components/main/utils.tsx
+++ public/components/main/utils.tsx
@@ -17,4 +17,4 @@ function parseData(parse) {
 const parseDate = timeParse("%Y-%m-%d");
 
 export function getData() {
-	const promiseMSFT = fetch("//rrag.github
+	const promiseMSFT = fetch("//rrag.github.io/react-stoc
